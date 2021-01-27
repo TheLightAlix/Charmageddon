987a76dbc8cc5c21701ad1bb30a2d5376a4aa775
anubis09 <60443037+anubis09@users.noreply.github.com>
2021-01-27 12:07:18 +0100

    Update map.hpp

--- classi mappa e macchina/map.hpp
+++ classi mappa e macchina/map.hpp
@@ -1,18 +1,21 @@
-
+//
+// Created by Lorenzo on 26/01/2021.
+//
 using namespace std;
-#include <iostream>	
+#include <iostream>
 
 #ifndef NOMEPROGETTO_MAP_HPP
 #define NOMEPROGETTO_MAP_HPP
 
-
-class map{
+class Map{
 protected:
     char matrix[116][21];
 public:
-    map();
+    Map();
     void print();
     void set(char, int, int);
+    void defGrass(int, int);
+    void createLineGrass(int);
 };
 
 #endif //NOMEPROGETTO_MAP_HPP
