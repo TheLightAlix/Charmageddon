a272d02a683359e71b40e702dd4ec6b18ed103de
anubis09 <60443037+anubis09@users.noreply.github.com>
2021-01-31 15:10:54 +0100

    Update map.hpp

--- classi mappa e macchina/map.hpp
+++ classi mappa e macchina/map.hpp
@@ -3,6 +3,8 @@
 //
 using namespace std;
 #include <iostream>
+#include <stdlib.h>
+#include <ctime>
 
 #ifndef NOMEPROGETTO_MAP_HPP
 #define NOMEPROGETTO_MAP_HPP
@@ -13,10 +15,17 @@ protected:
     int nOfLines=21;
     int nOfColumns=116;
     int horizon=9;
+    int colTopLeftRoad=22;//column where the top left road margin begins '/'
+    int colTopRightRoad=92;//column where the top right road margin begins '\'
+    int colBottomLeftRoad=12;//column where the bottom left road margin begins '/'
+    int colBottomRightRoad=102;//column where the bottom right road margin begins '\'
 public:
     Map();
     void print();
     void set(char, int, int);
+    void defGrass(int, int);
+    void createLineGrass();
+    void runGrass();
 };
 
 #endif //NOMEPROGETTO_MAP_HPP
