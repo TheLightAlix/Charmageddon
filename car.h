ab5da28c2935fb13414f461ae684c7508a8c02ff
anubis09 <60443037+anubis09@users.noreply.github.com>
2021-02-04 16:19:27 +0100

    Create car.h

---
+++ car.h
@@ -0,0 +1,19 @@
+#include "Map.h"
+#ifndef GIOCO2_0_CAR_H
+#define GIOCO2_0_CAR_H
+
+class Car {
+protected:
+    float relPos; //compreso tra -1 e 1, ed è posizione relativa rispetto al centro dello screen
+    float movement;
+    int xCarPosition(float, int);//serve per calcolare la posizione in cui verrà stampata la macchina.
+    colours colour; //decide il colore da dare alla macchina
+public:
+    Car(Map *,colours);
+    void printCar(Map *); //serve per stampare la macchina all'interno della mappa.
+    void moveCar(Map *); //serve per far muovere la macchiana a destra e sinistra.
+    float getMovement();
+    void setMovement(float);
+};
+
+#endif //GIOCO2_0_CAR_H
