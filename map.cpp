da60b5ba6d0ef9507c2360013d8806a68de33bff
anubis09 <60443037+anubis09@users.noreply.github.com>
2021-02-04 16:21:02 +0100

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -2,47 +2,60 @@
 // Created by Lorenzo on 02/02/2021.
 //
 
+
 #include "Map.h"
 Map::Map() {
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&csbi);
-    screenWidth=csbi.srWindow.Right;
-    screenHeight=csbi.srWindow.Bottom;
-    xMiddle=0.5;
-    roadWidth=0.85;
-    startLeftRoad=(xMiddle-roadWidth/2)*screenWidth;
-    startRightRoad=(xMiddle+roadWidth/2)*screenWidth;
+    screenWidth = csbi.srWindow.Right;
+    screenHeight = csbi.srWindow.Bottom;
+    xMiddle = 0.5;
+    roadWidth = 0.85;
+    startLeftRoad = (xMiddle-roadWidth/2)*screenWidth;
+    startRightRoad = (xMiddle+roadWidth/2)*screenWidth;
 }
 void Map::setCursor(short x,short y) {
     COORD position;
-    position.X=x;
-    position.Y=y;
+    position.X = x;
+    position.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),position);
 }
 void Map::setAndPrintChar(char c,short x, short y) {
     setCursor(x,y);
     cout<<c;
 }
-void Map::setAndPrintString(string st,short x,short y) {
+
+void Map::setAndPrintStrCol(string str, short x, short y, colours col=WHITE) {
+    switch(col) {
+            case BLUE:
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE);
+            break;
+        case GREEN:
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
+            break;
+        case RED:
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+            break;
+        default: ;
+    }
     setCursor(x,y);
-    cout<<st;
+    cout<<str;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),csbi.wAttributes);//ritorna alla normalità
 }
 void Map::printMap() {
-    for(int y=screenHeight;y>=screenHeight/2.3;y--){
-        for (int x=0;x<=screenWidth;x++){
-            if(x==(startLeftRoad+screenHeight-y)){
+    for(int y = screenHeight;y >= screenHeight/2.3;y--){ //il 2.3 serve per dare più di metà schermo alla strada
+        for (int x = 0;x <= screenWidth;x++){
+            if(x == (startLeftRoad+screenHeight-y)){
                 setAndPrintChar('/',x,y);
             }
-            else if(x==startRightRoad-screenHeight+y){
+            else if(x == (startRightRoad-screenHeight+y)){
                 setAndPrintChar('\\',x,y);
             }
         }
     }
-    setCursor(screenWidth,screenHeight);
 }
 
 void Map::setCarPos(int x) {
-    carPosition=x;
+    carPosition = x;
 }
 int Map::getScreenWidth() {
     return screenWidth;
