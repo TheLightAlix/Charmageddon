c3dff3a4eb8b6ab604c5ef6af0a22f2f9aa6f7fd
anubis09 <60443037+anubis09@users.noreply.github.com>
2021-02-04 19:27:14 +0100

    Update and rename car.h to player.h

--- player.h
+++ player.h
@@ -1,15 +1,16 @@
+
 #include "Map.h"
 #ifndef GIOCO2_0_CAR_H
 #define GIOCO2_0_CAR_H
 
-class Car {
+class Player {
 protected:
     float relPos; //compreso tra -1 e 1, ed è posizione relativa rispetto al centro dello screen
     float movement;
     int xCarPosition(float, int);//serve per calcolare la posizione in cui verrà stampata la macchina.
     colours colour; //decide il colore da dare alla macchina
 public:
-    Car(Map *,colours);
+    Player(Map *, colours);
     void printCar(Map *); //serve per stampare la macchina all'interno della mappa.
     void moveCar(Map *); //serve per far muovere la macchiana a destra e sinistra.
     float getMovement();
