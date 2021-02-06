86e3109ea52c5848cfc1e6f4df25981ec81ed16c
anubis09 <60443037+anubis09@users.noreply.github.com>
2021-02-06 15:18:22 +0100

    Update player.h

--- player.h
+++ player.h
@@ -5,16 +5,21 @@
 
 class Player {
 protected:
-    float relPos; //compreso tra -1 e 1, ed è posizione relativa rispetto al centro dello screen
+    float relPos; //compreso tra -1 e 1, ed è posizione relativa rispetto al centro dello screen -> 0 è centrata
     float movement;
+    string car[4]; //sprite della macchina
+    int carWidth;
     int xCarPosition(float, int);//serve per calcolare la posizione in cui verrà stampata la macchina.
     colours colour; //decide il colore da dare alla macchina
+    void resetAndPrint(Map *,int);
+    bool outOfRoad;
 public:
-    Player(Map *, colours);
+    Player(Map *,int,colours);
     void printCar(Map *); //serve per stampare la macchina all'interno della mappa.
     void moveCar(Map *); //serve per far muovere la macchiana a destra e sinistra.
     float getMovement();
     void setMovement(float);
+    int getCarWidth();
 };
 
 #endif //GIOCO2_0_CAR_H
