050be29e231ae76da4dd2542f68027606ad318f8
anubis09 <60443037+anubis09@users.noreply.github.com>
2021-02-06 15:18:54 +0100

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,45 +1,70 @@
 
 #include "Player.h"
-Player::Player(Map *map, colours col) {
+Player::Player(Map *map,int type,colours col) {
     relPos = 0.0;
     movement=0.05;
     colour=col;
+    switch(type){
+        case 1:
+            car[3]="  _____";
+            car[2]=" /_..._\\";
+            car[1]="(*[###]*)";
+            car[0]=" []   []";
+            carWidth=9;
+            break;
+        default:
+            car[3]="  ||+++||";
+            car[2]="    +++";
+            car[1]="||| +++ |||";
+            car[0]="|||+++++|||";
+            carWidth=11;
+            break;
+    }
 }
 int Player::xCarPosition(float rp, int screenWidth) {
-    return (screenWidth/2 + (int)(screenWidth*rp/2) - 5);
+    return (screenWidth/2 + (int)(screenWidth*rp/2) - carWidth/2);
 }
 
 void Player::printCar(Map *map) {
-    string car[4];
-    car[3]="  _____";
-    car[2]=" /_..._\\";
-    car[1]="(*[###]*)";
-    car[0]=" []   []";
     int screenWidth = map->getScreenWidth();
     int screenHeight = map->getScreenHeight();
     int xPos= xCarPosition(relPos,screenWidth);
     map->setCarPos(xPos);
     map->setAndPrintStrCol(car,4,xPos,screenHeight,colour);
-    map->setCursor(screenWidth,screenHeight);
+    map->setCursor(screenWidth,0);
 }
-
+void Player::resetAndPrint(Map *map,int xCarPos) {
+    system("cls");
+    if((xCarPos<map->getSLR())||(xCarPos+carWidth>map->getSRR())){
+        string danger[7];
+        danger[0] ="/_____________\\";
+        danger[1]=" /     .     \\";
+        danger[2]="  /    |    \\";
+        danger[3]="   /   |   \\";
+        danger[4]="    /  |  \\";
+        danger[5]="     / | \\";
+        danger[6]="      / \\";
+        map->setAndPrintStrCol(danger,7,map->getScreenWidth()/2-7,map->getScreenHeight()/2.8,RED);
+    }
+    map->printMap();
+    printCar(map);
+}
 void Player::moveCar(Map *map) {
     int screenWidth=map->getScreenWidth();
+    int xCarPos;
     if (GetAsyncKeyState(VK_LEFT) & (0x8000 != 0)) {
-        if(xCarPosition(relPos-movement,screenWidth)>=0){
+        xCarPos=xCarPosition(relPos-movement,screenWidth);
+        if(xCarPos>=0){
             relPos-=movement;
         }
-        system("cls");
-        map->printMap();
-        printCar(map);
+        resetAndPrint(map,xCarPos);
     }
     else if(GetAsyncKeyState(VK_RIGHT)& (0x8000 != 0)){
-        if(xCarPosition(relPos+movement,screenWidth)<=screenWidth-9){
+        xCarPos=xCarPosition(relPos+movement,screenWidth);
+        if(xCarPos<=screenWidth-carWidth){
             relPos+=movement;
         }
-        system("cls");
-        map->printMap();
-        printCar(map);
+        resetAndPrint(map,xCarPos);
     }
 }
 void Player::setMovement(float mv) {
@@ -49,3 +74,7 @@ void Player::setMovement(float mv) {
 float Player::getMovement() {
     return movement;
 }
+int Player::getCarWidth() {
+    return carWidth;
+}
+
