5be9792290e3eb048c7f1939c518ebc085198b8c
TheLightAlix <47418869+TheLightAlix@users.noreply.github.com>
2021-02-08 15:12:30 +0100

    Add files via upload

--- GAME_MAIN/InteractableObject.cpp
+++ GAME_MAIN/InteractableObject.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "InteractableObject.hpp"
+#include "Map.hpp"
+
+
+InteractableObject::InteractableObject() {
+
+
+}
+
+
+
+
+void InteractableObject::SetObjCoord(short x, short y) {
+
+    objCoord.X=x;
+    objCoord.Y=y;
+}
+
+
+void InteractableObject::SetSpeed(int newSpeed){
+
+    speed=newSpeed;
+}
+
+int InteractableObject::GetHitbox(int hitboxLenght){
+
+    if(hitboxLenght<2&&hitboxLenght>-1)
+        return hitbox[hitboxLenght];
+}
+
+void InteractableObject::PrintObj(Map *myMap, string myObj[]) {
+
+
+}
+
+void InteractableObject::SetHitPlayer(bool myHitplayer) {
+
+    hitPlayer=myHitplayer;
+}
+
+int InteractableObject::GetObjWidth() {
+    return objWidth;
+}
--- GAME_MAIN/InteractableObject.hpp
+++ GAME_MAIN/InteractableObject.hpp
@@ -6,16 +6,40 @@
 #define CHARMAGEDDON_INTERACTABLEOBJECT_HPP
 
 
+#include <windows.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 
 class InteractableObject{
 
 protected:
-    float movement;
+    int speed;
+    COORD objCoord;
+    int objWidth;
+    bool hitPlayer;
+    bool isBonus;
+    int hitbox[2];
+    int pointsExchange;
+
+
 
 public:
     InteractableObject();
+    void MoveObject(class Map* myMap);
+    void PrintObj(class Map* myMap,string myObj[]);
     void SetMovement(float myMovement);
+    void SetObjCoord(short x,short y);
+    void SetHitPlayer(bool myHitplayer);
+    void SetSpeed(int newSpeed);
+    string GetObj(int index);
+    int GetObjWidth();
     float GetMovement();
+    int GetHitbox(int hitboxLenght);
+
+
 
 };
 
