edaa217453148ca4ffbecd5daeec8a315a5165a5
Lorenzo Tozzi <60443037+anubis09@users.noreply.github.com>
2021-02-24 19:09:58 +0100

    Add files via upload

---
+++ Charmageddon_1/GameState.hpp
@@ -0,0 +1,95 @@
+//
+// Created by TheLi on 21/02/2021.
+//
+
+#ifndef CHARMAGEDDON_1_GAMESTATE_HPP
+#define CHARMAGEDDON_1_GAMESTATE_HPP
+
+#include <windows.h>
+#include <iostream>
+#include "Player.hpp"
+#include "InteractableObject.hpp"
+#include "Bonus.hpp"
+#include "Obstacle.hpp"
+#include "Menu.hpp"
+#include "Map.hpp"
+#include <thread>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+
+enum state{
+    MENU,
+    PLAYING,
+    GAMEOVER
+};
+
+struct ListSpawnedObject{
+
+    int pointsWhenSpawned;
+    short xObjSpawnCoord;
+    class InteractableObject* MyObject;
+    ListSpawnedObject* next;
+    ListSpawnedObject* pre;
+};
+typedef ListSpawnedObject* ListSpawnedObjectPtr;
+
+
+
+
+class GameState{
+
+protected:
+    class Map *myMap;
+    class Player *myPlayer;
+    ListSpawnedObjectPtr head;
+
+    bool bIdecreasedLvl;
+    int bonusChance;
+    int generalSpawnChance;
+    int points,pointsThisLvl;
+    int pointPerTickVariation;
+    float millisecToUpdatePoints,millisecToSpawn;
+    COORD rightSpawn;
+    COORD leftSpawn;
+    COORD midSpawn;
+    int xSpawn;
+    int ySpawn;
+    short currentLvl;
+    int basePointsToIncreaseLvl;
+    float levelUpScaling;
+    state currentState;
+    class Menu *MyMenu;
+    int maxLevel;
+    bool Chance(int myPercent);
+    void SwitchSpawnPos(int lessThanThree);
+
+
+
+public:
+
+    GameState();
+    //GameState();
+    void SetGameState(state myState);
+    void PointsProgression(class Player* myPlayer);
+    void AddPoints(int addedPoints);
+    void LvlIncrease();
+    void IncreaseDifficulty();
+    void InitializeSpwnCoord(class Map* myMap);
+    void NewNodeSetting(int pointsWhenSpawned,short xObjSpawnCoord,class InteractableObject* MyObject);
+    void SpawnObjects();
+    void RecyclingOldObjects();
+
+    int GetPoints();
+    short GetCurrentLvl();
+    int Clamp(int myNumber,int lower,int upper);
+    state GetCurrentState();
+
+
+};
+
+#endif //CHARMAGEDDON_1_GAMESTATE_HPP
---
+++ Charmageddon_1/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <windows.h>
+#include "Player.hpp"
+#include "GameState.hpp"
+
+
+using namespace std;
+
+void ShowConsoleCursor(bool showFlag);
+
+
+
+
+int main() {
+    ShowConsoleCursor(false);
+    GameState* MyGame = new GameState();
+    MyGame->SetGameState(MENU);
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+void ShowConsoleCursor(bool showFlag)
+{
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_CURSOR_INFO     cursorInfo;
+
+    GetConsoleCursorInfo(out, &cursorInfo);
+    cursorInfo.bVisible = showFlag; // set the cursor visibility
+    SetConsoleCursorInfo(out, &cursorInfo);
+}
